380c2933a75f353afffa542902f75a719cf16e0f
Swingster <150227275+Swingster@users.noreply.github.com>
2025-03-28 14:40:31 -0400

    Initial commit

---
+++ src/test/java/com/example/ears/EarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

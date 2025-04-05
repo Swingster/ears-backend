9157256c5cfeb6c193ca3497807dc9a51e5a0f7a
Swingster <150227275+Swingster@users.noreply.github.com>
2025-04-04 22:11:39 -0400

    updates

--- src/main/java/com/example/ears/EarsApplication.java
+++ src/main/java/com/example/ears/EarsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EarsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EarsApplication.class, args);
 	}
-
 }
